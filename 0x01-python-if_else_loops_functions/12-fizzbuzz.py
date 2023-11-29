2517e433a61bd3349aaeeb1409fb51a8881b4591
ayoubleass <ayoubleas@gmail.com>
2023-11-29 06:48:54 -0800

    Control flow

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    word = "fizzbuzz"
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{} ".format(word), end="")
+        elif i % 5 == 0:
+            print("{} ".format(word[4:]), end="")
+        elif i % 3 == 0:
+            print("{} ".format(word[:4]), end="")
+        else:
+            print("{:d} ".format(i),  end="")
