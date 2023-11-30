acf3f4302aa2e75f416c5fa593cc2b6806ea2525
ayoubleass <ayoubleas@gmail.com>
2023-11-30 00:38:37 -0800

    File: 5-variable_load.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import  hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    names.sort()
+
+
+    for name in names:
+        if name[0:2] == "__":
+            continue
+        else: 
+            print(name)
