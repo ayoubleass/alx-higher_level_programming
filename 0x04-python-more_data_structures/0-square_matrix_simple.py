52c2c48c1a158fb42d478f5c8ce7c97378b3f27b
ayoubleass <ayoubleas@gmail.com>
2023-12-06 00:59:42 -0800

    File: 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square = []
+    placeholder = []
+    for row in matrix:
+        for i in range(0, len(row)):
+            placeholder.append(row[i] * row[i])
+            if i == len(row) - 1:
+                square[len(square):] = [placeholder]
+                placeholder = []
+    return square
