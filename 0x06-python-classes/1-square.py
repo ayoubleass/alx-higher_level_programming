a84a3804279c06f207d78510891d6d3682ba16b9
ayoubleass <ayoubleas@gmail.com>
2023-12-19 04:11:27 -0800

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!//usr/bin/python3
+"""
+This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    Initializes the object.
+
+    Attributes:
+         size (int): The size of the square.
+    """
+    def __init__(self, size):
+            self.__size = size
+
