0e7ba2a557227bf83e62f15b5991b77420c95744
ayoubleass <ayoubleas@gmail.com>
2023-12-19 04:46:14 -0800

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+"""
+
+
+class Square:
+    """
+    Initializes a new instance of the Square class.
+
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area
+        """
+        return self.__size * self.__size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    Initializes a new instance of the Square class.
+
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    def area(self):
+        """
+        Returns the current square area
+        """
+        if not isinstance(self.size, int):
+            raise TypeError("size must be an integer")
+        return self.size * self.size
