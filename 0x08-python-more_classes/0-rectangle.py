70785c096d8c3fbceae42b1aae47494814cbed0c
ayoubleass <ayoubleas@gmail.com>
2024-01-04 04:13:46 -0800

    OOP first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is an empty class that represent a rectangle
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that represent a rectangle
+    """
+    pass
