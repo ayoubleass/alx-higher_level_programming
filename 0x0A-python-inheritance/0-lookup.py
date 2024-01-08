035c1e24dccee68a49446e5d75f350d0ecdc787e
ayoubleass <ayoubleas@gmail.com>
2024-01-08 06:44:54 -0800

    File: 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+    This modeule is a function that return a list containing attribute and methods of the passed object
+"""
 def lookup(obj):
     object_attr = [element for element in dir(obj)]
     return object_attr
