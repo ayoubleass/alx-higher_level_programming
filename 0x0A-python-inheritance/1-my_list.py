c9b288a612f16a0f443ef1dfe072c369e44f9729
ayoubleass <ayoubleas@gmail.com>
2024-01-08 07:58:52 -0800

    File: 1-my_list.py, tests/1-my_list.txt

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,12 @@ class MyList(list):
     """
         This class is a subclass of the list object
     """
+    def __init__(self):
+        """
+            bind the params passed
+        """
+        super().__init__(self)
+
     def print_sorted(self):
         """
             print the list sorted
