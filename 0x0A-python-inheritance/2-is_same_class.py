1711aa1a7d6fb32a51b26cb208768045ef1435b7
ayoubleass <ayoubleas@gmail.com>
2024-01-08 08:10:03 -0800

    File: 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module containe a function that  check if an object is derived
+    from a specifique class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Params:
+            object: obj
+            class : a_class
+        Return:
+            Boolean
+    """
+    return isinstance(obj, a_class):
