b3aa5ee76a6c5d6d8d4697ad538853fea05939af
ayoubleass <ayoubleas@gmail.com>
2024-01-09 07:59:51 -0800

    File: 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,9 @@
     This skript  adds all arguments to a Python list,
     and then save them to a file
 """
-if __name__ == "__main__":
 
+
+if __name__ == "__main__":
     from sys import argv
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
     load_from_json_file = __import__("6-load_from_json_file")
