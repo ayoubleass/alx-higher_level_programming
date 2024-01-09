a2d4c0956ef389a8fbf5328642023d241636f607
ayoubleass <ayoubleas@gmail.com>
2024-01-09 05:36:29 -0800

    File: 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module containe a single  function that read files
+"""
+
+
+def read_file(filename=""):
+    """
+        print the content of a file
+        Prams:
+            string : filename
+        Return:
+            void
+    """
+    with open(filename, 'r') as f:
+        print(f.read())
