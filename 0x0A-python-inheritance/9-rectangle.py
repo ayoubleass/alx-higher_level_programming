e7226f2da6673cf579c4e8b518f0a2fa75274e61
ayoubleass <ayoubleas@gmail.com>
2024-01-10 06:52:34 -0800

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    This module define Rectangle class
+"""
+
+mod = __import__("7-base_geometry")
+
+
+class Rectangle(mod.BaseGeometry):
+    """
+    This class define Rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        validate the params and bind them to the object/instance
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+            Rteunr the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+            return str
+        """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
