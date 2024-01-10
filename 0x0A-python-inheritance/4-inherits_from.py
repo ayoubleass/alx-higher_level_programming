695ac3a889b6c27eccdf8743b987d6a62a1d1259
ayoubleass <ayoubleas@gmail.com>
2024-01-10 05:41:27 -0800

    File: 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     """
         check if the obj base class is the a subclass of a_class
     """
-    if type(obj) != a_class and  isinstance(obj, a_class):
+    if type(obj) != a_class and isinstance(obj, a_class):
         return True
     return False
