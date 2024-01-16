a66edef400fecbbfcd2d227da4f8e179e99e6db8
ayoubleass <ayoubleas@gmail.com>
2024-01-16 05:02:45 -0800

    updating

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,8 +24,6 @@ class Rectangle(base.Base):
         """
 
         super().__init__(id)
-        self.validate("width", width)
-        self.validate("height", height)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -66,70 +64,3 @@ class Rectangle(base.Base):
     def y(self, value):
         self.validate('y', value)
         self.__y = value
-
-    def area(self):
-        """
-        Calculate the area of a rectangle
-
-        Return: integer
-        """
-        return self.__width * self.__height
-
-    def display(self):
-        for n in range(self.__y):
-            print('')
-        for num in range(self.__height):
-            print(" " * self.__x, end="")
-            print("#" * self.__width)
-
-    def __str__(self):
-        """
-        Return a string that contains all the class attr values
-        """
-        return "[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".format(
-                self.id, self.__x,
-                self.__y, self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        """
-        Updating the attributess with new values.
-        """
-        if len(args) > 0:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.__width = args[1]
-            if len(args) >= 3:
-                self.__height = args[2]
-            if len(args) >= 4:
-                self.__x = args[3]
-            if len(args) >= 5:
-                self.__y = args[4]
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """
-        Return a dictonary containing all attributes
-        """
-        rect_att = dict()
-        for k, v in self.__dict__.items():
-            if k.startswith("_"):
-                prefix = "_" + self.__class__.__name__ + "__"
-                k = k[len(prefix):]
-            rect_att[k] = v
-        return rect_att
-
-    def validate(self, name, value):
-        """
-        Check if the binding is done correctly
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if name == "width" or name == "height":
-            if value <= 0:
-                raise ValueError("{} must be > 0".format(name))
-        if name == "x" or name == "y":
-            if value < 0:
-                raise ValueError("{}  must be >= 0".format(name))
