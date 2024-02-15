caf6772bbfe6c45a903fcc6b4615f73690424097
ayoubleass <ayoubleas@gmail.com>
2024-02-15 03:12:55 -0800

    Update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records of the table second_table with a name.
-SELECT score, namee FROM `second_table` WHERE name!='' ORDER BY score DESC;
+SELECT score, namee FROM `second_table` WHERE name <>'' and name IS NOT NULL ORDER BY score DESC;
