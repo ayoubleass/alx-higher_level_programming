7a8a8bae6d638017860460f5183a370c30bb0f9a
ayoubleass <ayoubleas@gamil.com>
2024-03-13 06:32:21 -0700

    File: 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const newList = [];
-  for (let i = list.length - 1; i > 0; i--) {
-    newList.push(list[i]);
+  for (let i = 0; i < list.length / 2; i++) {
+    let tmp = list[i];
+    list[i] = list[list.length - 1 - i];
+    list[list.length - 1 - i] = tmp;
   }
-  return newList;
+  return list;
 };
