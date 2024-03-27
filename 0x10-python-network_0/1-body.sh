994babdc1ebf6812351a3ec5cf59e753bfa9a95f
ayoubleass <ayoubleas@gmail.com>
2024-03-27 05:09:00 -0700

    File: 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
-status=$(curl -sI "$1" | head -n 1 | cut -d ' ' -f 2); if [ "$status" -eq 200 ];then curl -sX GET "$1"; fi
+status=$(curl -sI "$1" | head -n 1 | cut -d ' ' -f 2); if [ "$status" -eq 200 ];then curl -sX GET "$1" -L; fi
