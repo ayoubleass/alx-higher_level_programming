7b86deb81f04632a769f481f743023aaaa564dcb
ayoubleass <ayoubleas@gmail.com>
2024-03-28 08:42:20 -0700

    file: 0-hbtn_status.py && 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This script sends a request to the URL and
+displays the value of the X-Request-Id
+variable found in the header of the response.
+"""
+
 
 import urllib.request
 import sys
