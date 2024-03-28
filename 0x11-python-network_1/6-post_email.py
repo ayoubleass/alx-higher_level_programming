484b5e81c972becc67a98c754c46a914319e4ac8
ayoubleass <ayoubleas@gmail.com>
2024-03-28 10:19:03 -0700

    File: 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={"email", email})
+    print(r.text)
