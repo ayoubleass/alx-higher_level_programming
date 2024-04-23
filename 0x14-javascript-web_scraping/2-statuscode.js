8a1a4b774a4f3cf8ab4aa67394184bbf1511bca3
ayoubleass <ayoubleas@gmail.com>
2024-04-23 05:27:15 -0700

    adding 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+request.get(url).on('response', (response) => {
+  console.log('code: ' + response.statusCode);
+});
