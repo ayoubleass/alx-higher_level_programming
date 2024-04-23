b30f463ec2d4480417385a52612417960b2f633b
ayoubleass <ayoubleas@gmail.com>
2024-04-23 08:06:55 -0700

    adding file 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,9 @@
 const url = process.argv[2];
 const request = require('request');
 const target = 'https://swapi-api.alx-tools.com/api/people/18/';
-let count = 0;
 request.get(url).on('response', (response) => {
   let body = '';
+  let count = 0;
   response.on('data', (data) => {
     body += data.toString();
   });
